e989da938404d3e7d4a8d5861a13dc73e95f1927
unknown <antuvhai@gmail.com>
2026-02-26 23:40:24 +0600

    update social logins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -155,11 +155,10 @@ export const auth = betterAuth({
       }
     },
   },
-  baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientSecret: process.env.GOOGLE_SECRET_ID as string,
       accessType: "offline",
       prompt: "select_account consent",
     },
